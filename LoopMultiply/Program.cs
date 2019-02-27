a6024abe17df235e9e7c960f3b049a7d9203a21c
Puja Grover <puja_grover@yahoo.com>
2019-02-26 23:05:19 -0600

    Class5HwLoopMultiply. Commit, Push and Sync

--- LoopMultiply/Program.cs
+++ LoopMultiply/Program.cs
@@ -6,12 +6,26 @@ namespace LoopMultiply
     {
         public static void Main()
         {
-            Console.Write("Enter a number: ");
 
             // TODO: Loop from 2 to 20 and multiply the loop value by the number the user entered and output the value each time. 
 
             // TODO: When the loop is finished, output the sum of all previous values.
 
+            int finalOutput = 0;
+
+            Console.WriteLine("Enter a number: ");
+            string numberInput = Console.ReadLine();
+            numberInput = numberInput.Trim();
+
+            int Input = int.Parse(numberInput);
+           
+                for (int i = 2; i < 21; i++)
+                {
+                    int individualOutput = i * Input;
+                    Console.WriteLine(individualOutput);
+                    finalOutput = individualOutput + finalOutput;
+                }
+            Console.WriteLine(finalOutput);
             Console.ReadLine();
         }
     }
